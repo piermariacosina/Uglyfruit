55a977f15509e636b0b186c52db629fea56e22e9
Piermaria Cosina <piermaria.cosina@gmail.com>
2012-10-15 17:29:27 +0200

    some fix

--- header.php
+++ header.php
@@ -74,12 +74,12 @@
 			<hgroup>
 				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
 				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+				
 			</hgroup>			
 				<?php get_search_form(); ?>
-			<nav id="access" role="navigation">
+			<!-- <nav id="access" role="navigation">
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-			</nav><!-- #access -->
+			</nav><!-- #access --> 
 		</div>
 	</header><!-- #branding -->
 	<div id="header-shade"></div>
