6c6a115208118477984b1c76e47b64c09c3992b5
elena mellano <elena.mellano@thedoers.com>
2012-10-18 12:41:26 +0200

    fix

--- single.php
+++ single.php
@@ -24,7 +24,7 @@ get_header(); ?>
 
 					<?php get_template_part( 'content', 'single' ); ?>
 					<span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ) ); ?></span>
-						<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
+					<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
 					</nav><!-- #nav-single -->
 					<?php comments_template( '', true ); ?>
 
