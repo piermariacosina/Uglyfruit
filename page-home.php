aed871bfa2779dd8de5dcd8af97bc32985a8890a
elena mellano <elena.mellano@thedoers.com>
2012-10-22 10:39:25 +0200

    page-home

---
+++ page-home.php
@@ -0,0 +1,27 @@
+<?php
+get_header(); ?>
+		<div class="left-shade"></div>
+		<div id="primary">
+			
+			<div class="orange_header">
+				<div class="circle">
+					<h1 id="title"> <?php the_field("blog-titolo","options")?></h1>
+				</div>
+				<p id=description><?php the_field("blog-descrizione","options")?></p>
+			</div>
+			
+			
+			<div id="content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'landing' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+				<div id="posts_list">
+					<?php do_action("list_posts"); ?>
+				</div>
+			</div><!-- #content -->
+		</div><!-- #primary -->
+	<div class="right-shade"></div>
+<?php get_footer(); ?>
